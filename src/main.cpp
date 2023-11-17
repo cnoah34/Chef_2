e791144b104c6ff66a8f00288173a653cb56e12b
cnoah34 <colbynoah@gmail.com>
2023-11-16 23:23:29 -0800

    Moved .cpp and .h files to the src folder

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <ctime>
+
+#include "game.h"
+
+int main()
+{
+    Game game;
+    game.run();
+
+    return 0;
+}
---
+++ src/player.h
@@ -0,0 +1,15 @@
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include <SFML/Graphics.hpp>
+
+class Game
+{
+    public:
+        Player(sf::Texture texture);
+
+    private:
+
+};
+
+#endif
